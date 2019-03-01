38c928b0a7080b7a3aa1bf703a55aeeb1492f8f8
Eduardo Alvarez <eduardo.a1993@gmail.com>
2019-02-28 22:44:25 -0300

    Hacer la primera llamada al servicio

--- react-ui/src/components/content_posts/index.js
+++ react-ui/src/components/content_posts/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import Post from '../../components/post/';
 
-const ContentPosts = () => {
-    return(
-        <Post />
-    );
+class ContentPosts extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            posts: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:3001/posts/", {
+            method: 'GET',
+            headers:{
+                "Authorization": "whatever-you-want",
+                "content-type" : "application/json"
+            }
+        }).then(res => res.json())
+        .then(response => {
+            this.setState({
+                posts: response,
+            });
+        })
+        .catch(error => console.error('Error:', error));
+    }
+
+    
+
+    render() {
+        return(
+            <Post />
+        );
+    }
 };
 
 export default ContentPosts;
