91aefb55e30289d80efefabf5d51671595cf9d4c
Eduardo Alvarez <eduardo.a1993@gmail.com>
2019-03-02 17:10:42 -0300

    incluir datos obtenidos desde la api, en el componente de post

--- react-ui/src/components/post/index.js
+++ react-ui/src/components/post/index.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const CardCategory = props => {
-    return(<Card>
-        <CardContent>
-            <Typography variant="h5" component="h2">
-                Título del contenido
-            </Typography>
-            
-            <Typography component="p">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            </Typography>
-        </CardContent>
-        <CardActions>
-            <Button size="small">Like</Button>
-            <Button size="small">Dislike</Button>
-        </CardActions>
-    </Card>);
+
+const CardCategory = ({ post }) => {
+    console.log(post);
+    return(
+        <Card>
+            <CardHeader
+                avatar={
+                    <Avatar aria-label="Recipe" >
+                        {post.author.charAt(0).toUpperCase()}
+                    </Avatar>
+                }
+                title={post.title}
+                subheader={post.timestamp}
+            />
+            <CardContent>
+                <Typography component="p">
+                    { post.body }
+                </Typography>
+            </CardContent>
+
+            <CardActions>
+                <Button size="small">Like</Button>
+                <Button size="small">Dislike</Button>
+            </CardActions>
+        </Card>);
 }
 export default CardCategory;
