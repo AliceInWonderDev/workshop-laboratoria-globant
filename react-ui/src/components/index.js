c27dd28517206bef08cbfff0da6b5e56ef539ba8
Eduardo Alvarez <eduardo.a1993@gmail.com>
2019-03-04 12:37:05 -0300

    Agregar nueva vista

--- react-ui/src/components/index.js
+++ react-ui/src/components/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Posts from '../views/posts/';
+import NewPost from '../views/new_post';
 
 const App = () => <BrowserRouter>
     <Switch>
         <Route path="/" exact component={Posts} />
+        <Route path="/new-post" exact component={NewPost} />
     </Switch>
 </BrowserRouter>
 
---
+++ react-ui/src/views/new_post/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NewPost = () => <h1>Nuevo Post</h1>
+
+export default NewPost;
