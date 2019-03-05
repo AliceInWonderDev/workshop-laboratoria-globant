8f1ba1f7e04ce2ecdbb7d78ef2586844356ca3aa
Eduardo Alvarez <eduardo.a1993@gmail.com>
2019-03-05 18:44:52 -0300

    Eliminar boton mas

--- react-ui/src/views/posts/index.js
+++ react-ui/src/views/posts/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Navbar from '../../components/navbar/';
 import ContentPosts from '../../components/content_posts/';
-import BottonTo from '../../components/bottons';
 
 const Posts = () => {
     return(
         <React.Fragment>
             <Navbar show/>
             <ContentPosts />
-            <BottonTo path="new-post"/>
         </React.Fragment>
     );
 }
